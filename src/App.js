92ca8d79989c87725f30526754ce7c8b7c0817fe
Ravi-Rsankar <ravisankar.r93@gmail.com>
2023-09-17 20:39:10 +0530

    react xstate integration for search

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import AppSearch from './Search';
+
+function App() {
+
+    return (
+        <div className="App">
+            {/* <Search /> */}
+            <AppSearch />
+        </div>
+    );
+}
+
+export default App;
